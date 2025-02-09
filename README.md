ab0b36917a2d1343f31399a458bf60899c45e9b2
ejsadiarin <ejs@ejsadiarin.com>
2025-02-09 22:09:32 +0800

    chore: add specs

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # CSNETWK MP
 
-
+`\join <server_ip> <port>` - joins a server given `<server_ip> <port>`
+`\leave` - disconnect to server
+`\register` - register unique handle or alias
+`\dir` - lists all files from server
+`\get` - download file from server 
+`\store` - upload file to server 
+`\?` - see all commands
--- main.go
+++ main.go
@@ -12,30 +12,43 @@ func main() {
 }
 
 func client() {
-	c, err := net.Dial("tcp", "localhost:6969")
+	conn, err := net.Dial("tcp", "localhost:6969")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer c.Close()
+	defer conn.Close()
 	s := "ejsadiarin"
 	fmt.Printf("eeeeeeeeeeee: %s\n", s)
 }
 
 func server() {
-	server, err := net.Listen("tcp", ":6969")
+	listener, err := net.Listen("tcp", ":6969")
 	if err != nil {
 		// slog.Info("server listening on port 6969")
 		log.Fatal(err)
 	}
 
 	fmt.Println("server listening on port 6969")
-	defer server.Close()
+	defer listener.Close()
 
 	for {
-		server.Accept()
-		go handleConnection()
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConnection(conn)
 	}
 }
 
-func handleConnection() {
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	// create buffer
+	buf := make([]byte, 1024) // 1 KB (for 1MB: 1024 * 1024)
+	// read data from conn and store it in buf
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// some processing...
+	fmt.Printf("Received data: %v\n", buf[:n])
 }
